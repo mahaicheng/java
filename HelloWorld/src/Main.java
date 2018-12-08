dcac15f210cc84491fc07f0297522bbf46fcdae1
mahaicheng <hchma@outlook.com>
2018-12-08 22:33:22 +0800

    add helloWorld

---
+++ HelloWorld/src/Main.java
@@ -0,0 +1,17 @@
+import com.sun.xml.internal.bind.v2.TODO;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 5;
+        int c = a + b;
+
+        //TODO: use new format
+        for (int i = a; i < b; ++i){
+            System.out.println(i);
+        }
+        System.out.print(c);
+        System.out.println("Hello World!");
+    }
+}
