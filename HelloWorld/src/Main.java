28116f7218c46c18e0e34ef862e8566f177d06e7
mahaicheng <hchma@outlook.com>
2018-12-09 14:42:46 +0800

    add fibonacci

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,1 +1,28 @@
-public class Main {    public static void main(String[] args) {        int a = 1;        int b = 5;        int c = a + b;        //TODO: use new format        for (int i = a; i < b; ++i){            System.out.println(i);        }        System.out.print(c);        System.out.println("Hello World!");        System.out.print(add(a, b));        System.out.println(fibonacci(40));        System.out.println(add(fibonacci(40), fibonacci(42)));    }    private static int add(int lhs, int rhs) {        return lhs + rhs;    }    private static int fibonacci(int i) {        final int i1 = 1;        if (i < i1) {            return 0;        }        if (i <= 2) {            return i1;        } else            return fibonacci(i - i1) + fibonacci(i - 2);    }}
+public class Main {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 5;
+        int c = a + b;
+
+        for (int i = a; i < b; ++i) {
+            System.out.println(i);
+        }
+        System.out.print(c);
+        System.out.println("Hello World!");
+
+        System.out.print(add(a, b));
+        System.out.println(fibonacci(40));
+        System.out.println(add(fibonacci(40), fibonacci(42)));
+    }
+
+    private static int add(int lhs, int rhs) {
+        return lhs + rhs;
+    }
+
+    private static int fibonacci(int i) {
+        if (i < 1) {
+            return 0;
+        }
+        return i <= 2 ? 1 : fibonacci(i - 1) + fibonacci(i - 2);
+    }
+}
